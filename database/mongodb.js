48069e5ad4eb5218bcb82465581e3bb135bd9c9c
Saurabh-314 <maxxxjob314@gmail.com>
2023-08-29 11:52:42 +0530

    add mongodb atlas link

--- database/mongodb.js
+++ database/mongodb.js
@@ -6,8 +6,8 @@ const username = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const url = process.env.MONGO_DB_URL;
 
-// const MONGO_URL = `mongodb+srv://${username}:${password}@${url}/miraki?retryWrites=true&w=majority`;
-const MONGO_URL = `mongodb://127.0.0.1:27017/maraki`;
+const MONGO_URL = `mongodb+srv://${username}:${password}@${url}/miraki?retryWrites=true&w=majority`;
+// const MONGO_URL = `mongodb://127.0.0.1:27017/maraki`;
 
 async function connect() {
 
